ba484a38054d84f575d9430af6202f858e7d379e
Kostas Minaidis <minaidisk@gmail.com>
2020-06-28 14:57:08 +0300

    03) Creating our base App Component.

--- index.js
+++ index.js
@@ -1,2 +1,9 @@
 import React from 'react';            // We import the React library in order to code in JSX
 import { render } from 'react-dom';   // The render method will put our JSX in the HTML page: index.html
+
+// Our main Application Container Component
+function App(){
+  return (
+    <div>App</div>
+  )
+}
