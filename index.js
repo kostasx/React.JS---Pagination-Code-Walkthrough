1c23343c0c86a8f1052d4b85c6f3739fc2b90dfb
Kostas Minaidis <minaidisk@gmail.com>
2020-06-28 15:30:08 +0300

    12) Cleaning up comments.

--- index.js
+++ index.js
@@ -1,17 +1,14 @@
-import React, { useState } from 'react';            // We import the useState Hook to add state to our Components
-import { render } from 'react-dom';   // The render method will put our JSX in the HTML page: index.html
+import React, { useState } from 'react';            
+import { render } from 'react-dom';   
 
-// Create a basic list of programming languages
 const list = [ "JavaScript", "TypeScript", "CoffeeScript", "PHP", "Python", "Go", "Ruby", "Java", "C#", "C++" ];
 
-// Create a Component to display each programming language
 function Post({ title }){
-  return <div>{ title }</div>;  // Display the title Prop value
+  return <div>{ title }</div>;  
 }
 
-// Our main Application Container Component
 function App(){
-  const [ posts ] = useState( list ); // We create a state variable and assign an initial value: list
+  const [ posts ] = useState( list ); 
   const prev = ()=>{
   }
   const next = ()=>{
@@ -19,14 +16,13 @@ function App(){
   }
   return (
     <div>
-      {/* Creating Next & Previous buttons along with a current page indicator */}
       <button onClick={ prev }>Prev</button> <span>1/4</span> <button onClick={ next }>Next</button>
       <hr/>
       {
-        posts.map( ( post, index ) => <Post key={ index } title={ post } /> ) /* Pass each list value to the Component as a Prop */
+        posts.map( ( post, index ) => <Post key={ index } title={ post } /> ) 
       }
     </div>
   )
 }
 
-render( <App/>, document.getElementById("root") ); // Render our App Component to the index.html page
+render( <App/>, document.getElementById("root") );
