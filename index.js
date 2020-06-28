6a8773837418cdbd62c587ac7b9f2913e16c6268
Kostas Minaidis <minaidisk@gmail.com>
2020-06-28 14:48:25 +0300

    02) Importing the basics: React and ReactDOM.render

---
+++ index.js
@@ -0,0 +1,2 @@
+import React from 'react';            // We import the React library in order to code in JSX
+import { render } from 'react-dom';   // The render method will put our JSX in the HTML page: index.html
