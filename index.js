107717bd9f6f04f9824cac3399cd0cd362147d19
Kostas Minaidis <minaidisk@gmail.com>
2020-06-28 17:03:08 +0300

    26) Final commented code

--- index.js
+++ index.js
@@ -12,39 +12,44 @@ NEXT STEPS?
 - Apply styling using Bootstrap 4
 
 */
-import React, { useState } from 'react';            
-import { render } from 'react-dom';   
+import React, { useState } from 'react'; // We import React to work with JSX and the useState Hook to add state to our Components            
+import { render } from 'react-dom'; // The render method will put our JSX in the HTML page: index.html
 
+// Create a basic list of programming languages
 const list = [ "JavaScript", "TypeScript", "CoffeeScript", "PHP", "Python", "Go", "Ruby", "Java", "C#", "C++" ];
 
+// Create a Component to display each programming language
 function Post({ title }){
-  return <div>{ title }</div>;  
+  return <div>{ title }</div>;  // Display the title Prop value
 }
 
-const postsPerPage = 3; 
-const totalPages = Math.ceil( list.length / postsPerPage ); 
+const postsPerPage = 3; // Adding variable which holds the "number of Posts to be displayed per Page" setting
+const totalPages = Math.ceil( list.length / postsPerPage ); // Calculating the total number of Pages. Play around with the postsPerPage variable at this stage
 
+// Our main Application Container Component
 function App(){
-  const [ posts ] = useState( list ); 
+  const [ posts ] = useState( list ); // We create a state variable and assign an initial value: list
   const [ currentPage, setCurrentPage ]= useState( 1 );
   const prev = ()=>{
-    if ( currentPage > 1 ){ setCurrentPage( currentPage - 1 ); } 
+    if ( currentPage > 1 ){ setCurrentPage( currentPage - 1 ); } // Decrementing current page index ensuring we don't get past 0
   }
   const next = ()=>{
-    if ( currentPage < totalPages ){ setCurrentPage( currentPage + 1 ); } 
+    if ( currentPage < totalPages ){ setCurrentPage( currentPage + 1 ); } // Incrementing current page index ensuring we don't get past the upper limit: totalPages
   }
-  let from = ( currentPage - 1 ) * postsPerPage; 
-  let upTo = from + postsPerPage;                
+  let from = ( currentPage - 1 ) * postsPerPage; // Start slicing from here
+  let upTo = from + postsPerPage;                // Stop slicing here
 
   return (
     <div>
+      {/* Creating Next & Previous buttons along with a current page indicator */}
       <button onClick={ prev }>Prev</button> <span>{currentPage}/{totalPages}</span> <button onClick={ next }>Next</button>
       <hr/>
       {
+        /* Slicing the posts array using the 'from'/'upTo' lower/upper limits: */
         posts.slice( from, upTo ).map( ( post, index ) => <Post key={ index } title={ post } /> ) 
       }
     </div>
   )
 }
 
-render( <App/>, document.getElementById("root") );
+render( <App/>, document.getElementById("root") ); // Render our App Component to the index.html page
